e36d9f52271541ffd4eb2bb6ed6dd530d8fddc29
steven12138 <18611209860@163.com>
2022-11-28 16:10:40 +0800

    include arco design

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import {createPinia} from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+// Arco Design
+import ArcoVue from "@arco-design/web-vue";
+import '@arco-design/web-vue/dist/arco.css';
+
 import './assets/main.css';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.use(ArcoVue);
 
 app.mount('#app');
